9848dca44118ee60d3b3eb5ba9627bbbe8304d59
Nazhier-Rijalana <nazhier9974@gmail.com>
2021-06-03 09:00:58 +0700

    round args

--- food/utils.py
+++ food/utils.py
@@ -5,7 +5,7 @@ type_2 = ['b_kar', 'kar_total']
 
 def count_nutrition(before, count, name):
 
-    data = round(before * (count/100))
+    data = round(before * (count/100), 2)
     if name == 'energi':
         ret = "{} {}".format(data, 'kal')
     elif name in type_1:
@@ -19,12 +19,12 @@ def count_nutrition(before, count, name):
 
 def convert_mg_to_g(name, nutrition):
     if name in type_1:
-        nutri = round(float(nutrition) / 1000)
+        nutri = round(float(nutrition) / 1000, 2)
         return nutri
     else:
         return float(nutrition)
 
 
 def count_presentation(nutrition, total):
-    presentation = round((nutrition / total) * 100)
+    presentation = round((nutrition / total) * 100, 2)
     return presentation
