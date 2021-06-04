40ac89e1738ae8a5504bb8c2e5e4df7a949373af
Nazhier-Rijalana <nazhier9974@gmail.com>
2021-06-04 14:41:15 +0700

    pagination

--- backend_bangkit/settings.py
+++ backend_bangkit/settings.py
@@ -122,6 +122,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20,
     'SERIALIZER_EXTENSIONS': {
         'USE_HASH_IDS': True,
         'HASH_IDS_SOURCE': 'user.HASH_IDS'
